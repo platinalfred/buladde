0e3b300699286e63f5530bfebaf5d730dc99431d
Allan Jesse Odeke <Allan Jesse Odeke>
2017-01-25 14:10:41 +0300

    Fixed table issue

--- client_transactions.php
+++ client_transactions.php
@@ -30,9 +30,12 @@ if(!isset($_GET['start_date'])){
 				<?php
 			}else{ ?>
 				<div class="table-responsive">
-				  <table class="table table-striped jambo_table bulk_action">
+				  <table id="datatable-buttons" class="table table-striped jambo_table bulk_action">
 					<thead>
 					  <tr class="headings">
+						<th>
+						  <input type="checkbox" id="check-all" class="flat">
+						</th>
 						<?php 
 						$header_keys = array("Account Number", "Transaction Type", "Amount", "Transaction Date", "Transacted By");
 						foreach($header_keys as $key){ ?>
@@ -41,15 +44,24 @@ if(!isset($_GET['start_date'])){
 						}
 						?>
 					  </tr>
+					  <tr>
+						<th class="bulk-actions" colspan="6">
+						  <a class="antoo" style="color:#fff; font-weight:500;">Bulk Actions ( <span class="action-cnt"> </span> ) <i class="fa fa-chevron-down"></i></a>
+						</th>
+					  </tr>
 					</thead>
 					<tbody>
 						<?php
 						$c = 1;
+						$cur_balance = 0;
 						foreach($client_transactions as $single){ ?>
 							<tr class="<?php if(($c%2) == 0){  echo "even"; }else{ echo "odd"; } ?> pointer">
+								<td class="a-center ">
+									<input type="checkbox" value="<?php echo $single['id']; ?>" class="flat" name="table_records">
+								</td>
 								<td class=" "><?php echo $accounts->findAccountNoByPersonNumber($single['person_number']); ?> </td>
 								<td class=" "><?php if($single['transaction_type'] == 1){ echo "Deposit"; }elseif($single['transaction_type'] == 2){ echo "Withdraw"; }elseif($single['transaction_type'] == 3){ echo "Shares"; }elseif($single['transaction_type'] == 4){ echo "Membership"; } ?></td>
-								<td class=" "><?php if($single['amount'] < 0){ echo "(".($single['amount'] * -1).")"; }else{ echo  $single['amount']; }; ?></td>
+								<td class=" "><?php $cur_balance += $single['amount']; if($single['amount'] < 0){ echo "(".($single['amount'] * -1).")"; }else{ echo  $single['amount']; }; ?></td>
 								<td><?php echo date("j F, Y", strtotime($single['transaction_date'])); ?> </td>
 								<td><?php echo $single['transacted_by']; ?></td>
 							</tr>
@@ -58,6 +70,13 @@ if(!isset($_GET['start_date'])){
 						}
 						?>
 					</tbody>
+					<tfoot>
+							<tr>
+								<th colspan="2">Total (UGX)</th>
+								<th><?php echo number_format($cur_balance,2,".",","); ?></th>
+								<th  colspan="3">&nbsp;</th>
+							</tr>
+					</tfoot>
 				  </table>
 				</div>
 				<?php 
